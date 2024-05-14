2b89f64186624d3ef5d014e862259dc180ebc29b
Clarisa Rodríguez <clarisacandia0108@gmail.com>
2024-05-14 02:00:33 -0300

    Cree clase Notificacion

---
+++ src/main/java/com/tallerwebi/dominio/notificacion/Notificacion.java
@@ -0,0 +1,63 @@
+package com.tallerwebi.dominio.notificacion;
+
+import com.tallerwebi.dominio.Usuario;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "notificaciones")
+public class Notificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    public Notificacion(Long id, String titulo, String descripcion) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+
+    public Notificacion() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
