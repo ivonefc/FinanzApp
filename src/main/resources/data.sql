ac0660452689e4bd9b0a55a7f28fb00280f13f1e
Clarisa Rodríguez <clarisacandia0108@gmail.com>
2024-05-15 23:40:16 -0300

    Agregue consultas al script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,11 +3,11 @@ INSERT INTO usuarios(id, email, password, rol, activo) VALUES(null, 'test@unlam.
 INSERT INTO tipos_movimiento(id, nombre) VALUES(null, 'INGRESO');
 INSERT INTO tipos_movimiento(id, nombre) VALUES(null, 'EGRESO');
 
+
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null,  'SUELDO', 1);
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'EXTRA', 1);
-INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'REMBOLSO', 1);
+INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'REEMBOLSO', 1);
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'REGALO', 1);
-INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'INGRESO_MOVIMIENTO_COMPARTIDO', 1);
 
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'RESTAURANTE', 2);
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'ENTRETENIMIENTO', 2);
@@ -15,4 +15,8 @@ INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'INDUMENTARI
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'TRANSPORTE', 2);
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'PAGO_DE_SERVICIOS', 2);
 INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'SUPERMERCADO', 2);
-INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'EGRESO_MOVIMIENTO_COMPARTIDO', 2);
+INSERT INTO categorias_movimiento(id, nombre, id_tipo) VALUES(null, 'SALUD_BELLEZA', 2);
+
+INSERT INTO movimientos(id, descripcion, fechayHora, monto, id_categoria, id_usuario) VALUES(null, 'Ropa', '2024-05-15', 30000, 7, 1);
+INSERT INTO movimientos(id, descripcion, fechayHora, monto, id_categoria, id_usuario) VALUES(null, 'Cobro', '2024-05-15', 100000, 1, 1);
+INSERT INTO movimientos(id, descripcion, fechayHora, monto, id_categoria, id_usuario) VALUES(null, 'Mercaderia', '2024-05-15', 20000, 10, 1);
