4367ad04b3995ea1f5b51ac22e52847c3d506c1a
vic-jun <vjunco304@alumno.unlam.edu.ar>
2024-06-18 18:52:38 -0300

    test para servicio usuario

--- src/test/java/com/tallerwebi/dominio/usuario/ServicioUsuarioTest.java
+++ src/test/java/com/tallerwebi/dominio/usuario/ServicioUsuarioTest.java
@@ -4,12 +4,19 @@ import com.tallerwebi.dominio.excepcion.ExcepcionBaseDeDatos;
 import com.tallerwebi.dominio.excepcion.ExcepcionCamposInvalidos;
 import com.tallerwebi.dominio.excepcion.UsuarioExistente;
 import com.tallerwebi.dominio.excepcion.UsuarioInexistente;
+import com.tallerwebi.presentacion.perfil.DatosEditarPerfil;
+import org.hamcrest.collection.IsMapWithSize;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -162,14 +169,107 @@ public class ServicioUsuarioTest {
         assertEquals("Base de datos no disponible", excepcion.getMessage());
     }
 
-//    @Test
-//    public void queAlSolicitarAlServicioModificarUsuarioLoHagaCorrectamente() {
-//        // ejecucion
-//        servicioUsuario.modificar(usuarioMock);
-//
-//        // validacion
-//        verify(repositorioUsuarioMock).modificar(usuarioMock);
-//    }
+    @Test
+    public void queAlSolicitarAlServicioModificarUsuarioLoHagaCorrectamente() throws ExcepcionBaseDeDatos, UsuarioInexistente, ExcepcionCamposInvalidos {
+        // preparacion
+        DatosEditarPerfil datosEditarPerfil = mock(DatosEditarPerfil.class);
+        when(datosEditarPerfil.getNombre()).thenReturn("nombre");
+        when(datosEditarPerfil.getApellido()).thenReturn("apellido");
+        when(datosEditarPerfil.getNombreUsuario()).thenReturn("nombreUsuario");
+        when(datosEditarPerfil.getEmail()).thenReturn("email@test");
+        when(datosEditarPerfil.getPais()).thenReturn("pais");
+        when(datosEditarPerfil.getTelefono()).thenReturn(1234567890L);
+        when(datosEditarPerfil.getFechaNacimiento()).thenReturn(null);
+        when(datosEditarPerfil.getId()).thenReturn(1L);
+
+        Usuario usuarioMock = mock(Usuario.class);
+        when(repositorioUsuarioMock.obtenerUsuarioPorId(1L)).thenReturn(usuarioMock);
+        when(usuarioMock.getNombre()).thenAnswer(invocation -> datosEditarPerfil.getNombre());
+        when(usuarioMock.getApellido()).thenAnswer(invocation -> datosEditarPerfil.getApellido());
+        when(usuarioMock.getNombreUsuario()).thenAnswer(invocation -> datosEditarPerfil.getNombreUsuario());
+        when(usuarioMock.getEmail()).thenAnswer(invocation -> datosEditarPerfil.getEmail());
+        when(usuarioMock.getPais()).thenAnswer(invocation -> datosEditarPerfil.getPais());
+        when(usuarioMock.getTelefono()).thenAnswer(invocation -> datosEditarPerfil.getTelefono());
+        when(usuarioMock.getFechaNacimiento()).thenAnswer(invocation -> datosEditarPerfil.getFechaNacimiento());
+
+        // ejecucion
+        servicioUsuario.modificar(datosEditarPerfil);
+
+        // validacion
+        verify(repositorioUsuarioMock).modificar(usuarioMock);
+        assertEquals(datosEditarPerfil.getNombre(), usuarioMock.getNombre());
+        assertEquals(datosEditarPerfil.getApellido(), usuarioMock.getApellido());
+        assertEquals(datosEditarPerfil.getNombreUsuario(), usuarioMock.getNombreUsuario());
+        assertEquals(datosEditarPerfil.getEmail(), usuarioMock.getEmail());
+        assertEquals(datosEditarPerfil.getPais(), usuarioMock.getPais());
+        assertEquals(datosEditarPerfil.getTelefono(), usuarioMock.getTelefono());
+        assertEquals(datosEditarPerfil.getFechaNacimiento(), usuarioMock.getFechaNacimiento());
+    }
+
+    @Test
+    public void queAlSolicitarAlServicioModificarUsuarioLanceExcepcionUsuarioInexistente() throws ExcepcionBaseDeDatos, UsuarioInexistente, ExcepcionCamposInvalidos {
+        // preparacion
+        DatosEditarPerfil datosEditarPerfil = mock(DatosEditarPerfil.class);
+        when(datosEditarPerfil.getId()).thenReturn(1L);
+        when(repositorioUsuarioMock.obtenerUsuarioPorId(1L)).thenReturn(null);
+
+        // ejecucion
+        UsuarioInexistente e = assertThrows(UsuarioInexistente.class, () -> {
+            servicioUsuario.modificar(datosEditarPerfil);
+        });
+
+        // validacion
+        assertEquals("No se encontro usuario", e.getMessage());
+        verify(repositorioUsuarioMock).obtenerUsuarioPorId(1L);
+    }
+
+    @Test
+    public void queAlSolicitarAlServicioModificarUsuarioLanceExcepcionBaseDeDatos() throws ExcepcionBaseDeDatos, UsuarioInexistente, ExcepcionCamposInvalidos {
+        // preparacion
+        DatosEditarPerfil datosEditarPerfil = mock(DatosEditarPerfil.class);
+        when(datosEditarPerfil.getId()).thenReturn(1L);
+        when(repositorioUsuarioMock.obtenerUsuarioPorId(1L)).thenThrow(new ExcepcionBaseDeDatos());
+
+        // ejecucion
+        ExcepcionBaseDeDatos excepcion = assertThrows(ExcepcionBaseDeDatos.class, () -> {
+            servicioUsuario.modificar(datosEditarPerfil);
+        });
+
+        // validacion
+        assertEquals("Base de datos no disponible", excepcion.getMessage());
+        verify(repositorioUsuarioMock).obtenerUsuarioPorId(1L);
+    }
+
+    @Test
+    public void queAlSolicitarAlServicioModificarUsuarioLanceExcepcionCamposInvalidos() throws ExcepcionBaseDeDatos, UsuarioInexistente, ExcepcionCamposInvalidos {
+        // preparacion
+        DatosEditarPerfil datosEditarPerfil = mock(DatosEditarPerfil.class);
+        Map<String, String> erroresEsperados = new HashMap<>();
+        erroresEsperados.put("nombre", "El campo es requerido");
+        erroresEsperados.put("apellido", "El campo es requerido");
+        erroresEsperados.put("email", "El campo es requerido");
+        erroresEsperados.put("nombreUsuario", "El campo es requerido");
+        erroresEsperados.put("pais", "El campo es requerido");
+        erroresEsperados.put("telefono", "El campo es requerido");
+        erroresEsperados.put("fechaNacimiento", "El campo es requerido");
+
+        doThrow(new ExcepcionCamposInvalidos(erroresEsperados)).when(datosEditarPerfil).validarCampos();
+
+        // ejecucion
+        ExcepcionCamposInvalidos excepcion = assertThrows(ExcepcionCamposInvalidos.class, () -> {
+            servicioUsuario.modificar(datosEditarPerfil);
+        });
+
+        // validacion
+        assertThat(excepcion.getErrores(), IsMapWithSize.aMapWithSize(7));
+        assertThat(excepcion.getErrores(), hasEntry("nombre", "El campo es requerido"));
+        assertThat(excepcion.getErrores(), hasEntry("apellido", "El campo es requerido"));
+        assertThat(excepcion.getErrores(), hasEntry("email", "El campo es requerido"));
+        assertThat(excepcion.getErrores(), hasEntry("nombreUsuario", "El campo es requerido"));
+        assertThat(excepcion.getErrores(), hasEntry("pais", "El campo es requerido"));
+        assertThat(excepcion.getErrores(), hasEntry("telefono", "El campo es requerido"));
+        assertThat(excepcion.getErrores(), hasEntry("fechaNacimiento", "El campo es requerido"));
+    }
 
     @Test
     public void queAlSolicitarAlServicioObtenerUsuarioPorIdLoHagaCorrectamente() throws ExcepcionBaseDeDatos, UsuarioInexistente {
